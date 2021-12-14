54a69ef45b2a31e600a826d871bee147e3e1258a
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-13 21:05:36 -0600

    Add files via upload

---
+++ Dec13.js
@@ -0,0 +1,18 @@
+const firstName = 'Jazmine';
+const lastName = 'Abdilahi';
+console.log(firstName + '' + lastName);
+
+const jonas = "I'm" + firstName + ', a' + (year - birthYear) + ' year old ' + job + '!';
+
+const jazmineNew = 'I'm $(firstName), a ${ year - birthYear } yearold $(job)
+console.log(jazmineNew);
+
+console.log('just a regular string...');
+
+console.log('string with \n\
+mustiple \n\
+lines')
+
+console.log('string
+multiple
+lines')
